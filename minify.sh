3c817cf87dee4e57f60f0c5bcbf5feeb5954e784
Jacky Tea <44061647+JackyTea@users.noreply.github.com>
2023-11-02 03:24:15 -0400

    refactor(page): embed svgs into document

--- minify.sh
+++ minify.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 HTML_FILE_DIR_NAME=index.html
-CSS_FILE_DIR_NAME=index.css
 
 html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-js --minify-css --html5 true $HTML_FILE_DIR_NAME -o $HTML_FILE_DIR_NAME
 echo "[INFO]: $HTML_FILE_DIR_NAME successfully minified!"
-
-cssnano $CSS_FILE_DIR_NAME $CSS_FILE_DIR_NAME
-echo "[INFO]: $CSS_FILE_DIR_NAME successfully minified!"
