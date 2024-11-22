467831640be6b3839a8c70589ccec7e5fd4864e7
Jacky Tea <44061647+JackyTea@users.noreply.github.com>
2024-11-21 20:53:08 -0500

    feat(portfolio): initial commit

---
+++ scripts/minify.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ..
+
+HTML_FILE_DIR_NAME=index.html
+CSS_FILE_DIR_NAME=styles/styles.css
+
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-css true $HTML_FILE_DIR_NAME -o $HTML_FILE_DIR_NAME
+echo "[INFO]: $HTML_FILE_DIR_NAME successfully minified!"
+
+cssnano $CSS_FILE_DIR_NAME $CSS_FILE_DIR_NAME
+echo "[INFO]: $CSS_FILE_DIR_NAME successfully minified!"
