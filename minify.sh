ab00ffb988522c4a16881eea52ad2c0659618177
Jacky Tea <44061647+JackyTea@users.noreply.github.com>
2025-10-02 15:49:18 -0400

    chore(assets): minified file names

--- minify.sh
+++ minify.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-cd ..
-
 HTML_FILE_DIR_NAME=index.html
-CSS_FILE_DIR_NAME=styles/styles.css
+CSS_FILE_DIR_NAME=index.css
 
 html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-js --minify-css --html5 true $HTML_FILE_DIR_NAME -o $HTML_FILE_DIR_NAME
 echo "[INFO]: $HTML_FILE_DIR_NAME successfully minified!"
