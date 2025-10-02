9d03cadb8bb81d0c3ef211926e319c7cc7ba28fb
Jacky <44061647+jackytea@users.noreply.github.com>
2025-10-02 14:49:18 -0400

    chore(scripts): aggressive minification strategy (#2)

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -5,7 +5,7 @@ cd ..
 HTML_FILE_DIR_NAME=index.html
 CSS_FILE_DIR_NAME=styles/styles.css
 
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-css true $HTML_FILE_DIR_NAME -o $HTML_FILE_DIR_NAME
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-js --minify-css --html5 true $HTML_FILE_DIR_NAME -o $HTML_FILE_DIR_NAME
 echo "[INFO]: $HTML_FILE_DIR_NAME successfully minified!"
 
 cssnano $CSS_FILE_DIR_NAME $CSS_FILE_DIR_NAME
